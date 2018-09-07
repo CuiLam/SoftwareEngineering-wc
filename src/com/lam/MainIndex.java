d62be06468877d84ba08e844e872f7ad78b20cea
3216004715 <506933566@qq.com>
2018-09-07 20:28:01 +0800

    chore(*):增加路径通配符识别功能

--- src/com/lam/MainIndex.java
+++ src/com/lam/MainIndex.java
@@ -1,16 +1,110 @@
 package com.lam;
 
-import static com.lam.util.FileUtil.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.lam.util.FileUtil.getFileCharsCount;
+import static com.lam.util.FileUtil.getFileLine;
+import static com.lam.util.FileUtil.getFileStringCount;
+import static com.lam.util.FileUtil.isDirectory;
+import static com.lam.util.FileUtil.isFile;
+import static com.lam.util.FileUtil.printFileMoreDate;
 
 public class MainIndex {
 
-    public static final int ARGS_PARAM = 0;
-    public static final int ARGS_PATH = 1;
-
     public static void main(String[] args) {
-        String param = args[ARGS_PARAM];
-        String path = args[ARGS_PATH];
+        MainIndex main = new MainIndex();
+        //处理参数
+        List<String> params = new ArrayList<>();
+        for (String s : args) {
+            if (s.startsWith("-")) {
+                params.add(s);
+            } else if (isFile(s)) {
+                for (String param : params)
+                    main.operSingleFile(s, param);
+            } else if (isDirectory(s)) {
+                for (String param : params)
+                    main.operFiles(s, param, null);
+            } else {
+                //s可能是通配符
+                String currentPath = main.getCurrentPath();
+                for (String param : params)
+                    main.operFiles(currentPath, param, getRegex(s));
+//                String[] paths = new File(currentPath).list();
+//                if (paths == null) {
+//                    System.out.println("当前目录文件夹为空！");
+//                    return;
+//                }
+//                for (String p : paths) {
+//
+//                }
+//                System.out.println("参数有误！");
+                return;
+            }
+        }
+    }
+
+    private static String getRegex(String string) {
+        String[] strings1 = string.split("\\*");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings1.length; ++i) {
+            if (i != 0 && !strings1[i].equals(""))
+                stringBuilder.append("\\S*");
+            stringBuilder.append(strings1[i]);
+//            System.out.println("--" + strings1[i] + "--");
+        }
+//        System.out.println(stringBuilder);
+        String[] strings2 = stringBuilder.toString().split("\\?");
+        stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings2.length; ++i) {
+            if (i != 0)
+                stringBuilder.append("\\S");
+            stringBuilder.append(strings2[i]);
+        }
+//        System.out.println(stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    private String getCurrentPath() {
+        String line = null;
+        try {
+            Process ps = Runtime.getRuntime().exec("pwd");
+            BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+            line = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    private void operFiles(String path, String param, String regex) {
+        String[] files = new File(path).list();
+        if (files == null) {
+            System.out.println("文件夹为空");
+            return;
+        }
+        for (String p : files) {
+            String truePath = path + "/" + p;
+            if (isDirectory(truePath)) {
+                operFiles(truePath, param, regex);
+            } else if (isFile(truePath)) {
+                if (regex != null && !Pattern.matches(regex, p))
+                    continue;
+                operSingleFile(truePath, param);
+            } else {
+                System.out.println("路径有误!");
+            }
+        }
+    }
+
+    private void operSingleFile(String path, String param) {
         int count;
+        System.out.println(path);
         switch (param) {
             case "-l":  //行数
                 count = getFileLine(path);
@@ -32,6 +126,9 @@ public class MainIndex {
                     System.out.println("该文件是空文件");
                 else
                     System.out.println("字符数：" + count);
+                break;
+            case "-a":  //更复杂的数据（代码行 / 空行 / 注释行）
+                printFileMoreDate(path);
                 break;
         }
     }
--- src/com/lam/util/FileUtil.java
+++ src/com/lam/util/FileUtil.java
@@ -11,6 +11,7 @@ public class FileUtil {
 
     /**
      * 判断某路径是否单个文件
+     *
      * @param path 路径
      * @return 如果是单个文件返回true
      */
@@ -21,6 +22,7 @@ public class FileUtil {
 
     /**
      * 判断某路径是否是文件夹
+     *
      * @param path 路径
      * @return 如果是文件夹返回true
      */
@@ -31,6 +33,7 @@ public class FileUtil {
 
     /**
      * 读取文件行数
+     *
      * @param path 文件路径
      * @return 返回文件行数
      */
@@ -49,6 +52,7 @@ public class FileUtil {
 
     /**
      * 计算字符串中单词个数
+     *
      * @param string 原字符串
      * @return 返回单词个数
      */
@@ -67,6 +71,7 @@ public class FileUtil {
 
     /**
      * 获取文件单词个数
+     *
      * @param path 文件路径
      * @return 返回文件单词个数
      */
@@ -86,6 +91,7 @@ public class FileUtil {
 
     /**
      * 计算文件字符数
+     *
      * @param path 文件路径
      * @return 返回文件字符数
      */
@@ -105,6 +111,7 @@ public class FileUtil {
 
     /**
      * 计算文件空行行数
+     *
      * @param path 文件路径
      * @return 返回空行行数
      */
@@ -127,6 +134,7 @@ public class FileUtil {
 
     /**
      * 判断字符串是否是注释行
+     *
      * @param line 字符串
      * @return 如果是返回true
      */
@@ -135,7 +143,7 @@ public class FileUtil {
         //如果包含这些表示可能是注释行
         //包含并且注释符号前全为非字符的一定是注释行
         if (line.contains("//")) {
-            return Pattern.matches(regex, line.split("//")[0]);
+            return line.split("//").length == 0 || Pattern.matches(regex, line.split("//")[0]);
         } else if (line.contains("/**")) {
             return Pattern.matches(regex, line.split("//*/*")[0]);
         } else if (line.contains("*/")) {
@@ -145,13 +153,14 @@ public class FileUtil {
         } else if (line.contains("--!>")) {
             return Pattern.matches(regex, line.split("//")[0]);
         } else if (line.contains("*")) {
-            return Pattern.matches(regex, line.split("/*")[0]);
+            return Pattern.matches(regex, line.split("//*")[0]);
         }
         return false;
     }
 
     /**
      * 计算文件注释行行数
+     *
      * @param path 文件路径
      * @return 返回注释行行数
      */
@@ -171,6 +180,7 @@ public class FileUtil {
 
     /**
      * 计算文件代码行行数
+     *
      * @param path 文件路径
      * @return 返回代码行行数
      */
@@ -183,11 +193,43 @@ public class FileUtil {
             while ((line = bufferedReader.readLine()) != null) {
                 //当去除首位空白之后若长度大于1则当做代码行
                 String s = line.trim();
-                count += (s.length() > 1 ? 1 : 0);
+                count += (s.length() > 1 && !isNoteLine(s) ? 1 : 0);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return count;
+    }
+
+    public static void printFileMoreDate(String path) {
+        int emptyLine = 0;
+        int noteLine = 0;
+        int codeLine = 0;
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(path));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (isNoteLine(line)) {
+//                    System.out.println("--note--" + line);
+                    ++noteLine;
+                } else if (line.trim().length() <= 1){
+//                    System.out.println("--empty-- " + line);
+                    ++emptyLine;
+                } else {
+//                    System.out.println("--code-- " + line);
+                    ++codeLine;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (emptyLine + noteLine + codeLine == 0) {
+            System.out.println("该文件是空文件");
+        } else {
+            System.out.println("空行：" + emptyLine);
+            System.out.println("代码行：" + codeLine);
+            System.out.println("注释行：" + noteLine);
+        }
     }
 }
